12fbfbfb097743f78a0cf54eebd3efcca8ae1dd2
clxmente <solorioeclem164@gmail.com>
2021-10-02 00:16:22 -0700

    padded message func

--- cs-sketch.js
+++ cs-sketch.js
@@ -1,8 +1,23 @@
-// cs-sketch.js; P5 key animation & input fcns.  // CF p5js.org/reference
-// Time-stamp: <2021-09-28 16:54:18 Chuck Siska>
+// Author Information
+    // Team Name: Temp
+        // Name1 : email
+        // Name2 : email
+        // Name3 : email
+        // Name4 : email
 
-// Make global g_canvas JS 'object': a key-value 'dictionary'.
-var g_canvas = { cell_size:20, wid:32, hgt:24 }; // JS Global var, w canvas size info.
+// File Information
+    // File Name: cs-sketch.js
+    /*
+        This file contains the functions that handle the validation, encryption,
+        display functions, etc. Most of the functionality behind this program
+        comes from this file.
+    */
+
+//TODO: Header information (author, description)
+//TODO: old text stays on grid after submitting without refresh
+//TODO: don't execute with invalid password.
+
+var g_canvas = { cell_size:20, wid:40, hgt:20 }; // JS Global var, w canvas size info. //? wid:50 to make grid 5 sections of 8 cells.
 
 function setup() // P5 Setup Fcn
 {
@@ -10,25 +25,38 @@ function setup() // P5 Setup Fcn
     let width = sz * g_canvas.wid;  // Our 'canvas' uses cells of given size, not 1x1 pixels.
     let height = sz * g_canvas.hgt;
     createCanvas(width, height);  // Make a P5 canvas.
-    draw_grid(20, 20, 'white', 'blue');
+    draw_grid(25, 8, 'white', 'yellow');
 }
+
 
 // Callback to get Input-box data.
 function retrieve_input()
 {
     var messageFieldInput = document.getElementById("messageInput").value; // Get data from messageInput box.
-    var passwordFieldInput = document.getElementById("passwordInput").value; // Get data from passwordInput box.
+    let padded_message = pad_message(messageFieldInput); // pad message func call.
+    console.log("PADDED MESSAGE: |" + padded_message + "|");
+    let block_one = "1" + padded_message.substring(0,7);
+    let block_two = "2" + padded_message.substring(7,14);
+    let block_three = "3" + padded_message.substring(14,21);
+    let block_four = "4" + padded_message.substring(21,27); //TODO: append the ascii code of length + 32?
 
+    let prepared_message = block_one + block_two + block_three + block_four;
+
+
+ 
+    var passwordFieldInput = document.getElementById("passwordInput").value; // Get data from passwordInput box.
+    
     console.log( "Password inputted = " + passwordFieldInput ); // Show data in F12 Console output.
     let validity = isPasswordValid(passwordFieldInput); // store T/F value whether password follows comprehensive8.
     console.log("Password Valid: " + validity); // output the result of password validity.
-
+    
+    // set the formatting for text
     stroke(0);
     fill(255, 0, 0);
-    textSize(15);
-    for (i in messageFieldInput)
+    textSize(17);
+    for (i in prepared_message)
     {
-        text(messageFieldInput[i], (6+(i*20)), 15);
+        text(prepared_message[i], (8+(i*25)), 20); // draw the text for each character in message.
     }
 }
 
@@ -40,22 +68,32 @@ function isPasswordValid(plaintext)
     let lower_ptrn = new RegExp("[a-z]");
     let digit_ptrn = new RegExp("[0-9]");
     let symbol_ptrn = new RegExp("[!\"#$%&'()*+,-./:;<=>?@\\[\\]\\\\^_`{|}~]"); // double escape brackets. Triple escape backslash \ to match.
-
+    
     invalid = 0;
     while (invalid == 0) 
     {
         if (plaintext.length != 8) {invalid++; break;} // Check if the length is the correct size. Must be 8.
-
+        
         if (!upper_ptrn.test(plaintext)) {invalid++; break;} // Check if string contains uppercase.
-
+        
         if (!lower_ptrn.test(plaintext)) {invalid++; break;} // Check if string contains lowercase.
-
+        
         if (!digit_ptrn.test(plaintext)) {invalid++; break;} // Check if string contains a digit 0-9
-
+        
         if (!symbol_ptrn.test(plaintext)) {invalid++; break;} // Check if string contains a symbol
-
+        
         break; // passed all tests
     }
-
+    
     return (invalid == 0); // True if valid. False if invalid.
+}
+
+// Function to pad message with spaces if length less than 27.
+function pad_message(message)
+{
+    if (message.length == 27) return message; // no padding if 27 chars long
+
+    //? Allow message length of 0?
+    message = message + "                          "; // 26 spaces to pad if message is 1 char long. 
+    return (message.substring(0,27)); // return the first 27 chars of the string.
 }
