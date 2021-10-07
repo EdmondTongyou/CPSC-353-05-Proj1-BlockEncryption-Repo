0cd1e646811b5ea1f1cb4a84d899bf60112daf02
clxmente <solorioeclem164@gmail.com>
2021-10-06 21:33:36 -0700

    drawing encrypted message on grid

--- cs-sketch.js
+++ cs-sketch.js
@@ -15,12 +15,6 @@
         comes from this file.
     */
 
-//TODO: old text stays on grid after submitting without refresh
-//TODO: ***ENCRYPTION FUNCTION***
-//TODO: show encrypted message under prepared message
-//TODO: check if decryption works. Should be the same method as encrypt.
-//TODO: dictionary word check for password.
-
 var g_canvas = { cell_size:20, wid:40, hgt:20 }; // JS Global var, w canvas size info. //? wid:50 to make grid 5 sections of 8 cells.
 
 function setup() // P5 Setup Fcn
@@ -36,49 +30,53 @@ function setup() // P5 Setup Fcn
 // Callback to get Input-box data.
 function retrieve_input()
 {
+    // get msg input
     var messageFieldInput = document.getElementById("messageInput").value; // Get data from messageInput box.
-    //? Allow msg length of 0?
     var orig_msg_length = messageFieldInput.length;
-    console.log("Original Message Length: " + orig_msg_length);
 
-    //!
-    //? Alert box or show html | could also turn this into a function.
-    var error = document.getElementById("msgError");
+    // Display error message and exit func if no msg.
     if (orig_msg_length == 0) {
-        error.textContent = "Please enter a message";
-        error.style.color = "red";
-        //alert("Please enter a message.");
-        return;
-    } else {error.textContent = "";}
-    //!
+        show_error("msgError", "Please enter a message.");
+        return; // don't execute rest of code if error.
+    } else {
+        show_error("msgError", ""); // clear txt if msg input fixed.
+    }
 
     var prepared_message = prepareMessage(messageFieldInput, orig_msg_length); // prepare msg func call.
-    console.log("PREPARED MESSAGE: |" + prepared_message + "|"); // show prepared msg between |
+    console.log("Prepared MSG: " + prepared_message);
 
+    // get pw input
     var pw_field_input = document.getElementById("passwordInput").value; // Get data from passwordInput box.
-    console.log( "Password inputted = " + pw_field_input ); // Show data in F12 Console output.
     let validity = isPasswordValid(pw_field_input); // store T/F value whether password follows comprehensive8.
-    console.log("Password Valid: " + validity); // output the result of password validity.
 
-    //!
-    error = document.getElementById("pwError");
+    // Display error message and exit func if invalid pw.
     if (!validity) {
-        error.textContent = "Please enter a valid password";
-        error.style.color = "red";
-        //alert("Please enter a valid password.");
+        show_error("pwError", "Please enter a valid password.");
         return;
-    } else {error.textContent = "";}
-    //!
+    } else {
+        show_error("pwError", ""); // clear txt if pw input fixed.
+    }
 
     // set the formatting for text
     stroke(0);
     fill(255, 0, 0);
     textSize(17);
-     // draw the text for each character in prepared_message.
+    // draw the text for each character in prepared_message.
     for (i in prepared_message)
     {
         text(prepared_message[i], (8+(i*25)), 20);
     }
+
+    var encoded_msg = encrypt(prepared_message, pw_field_input);
+    console.log("Encoded message: " + encoded_msg);
+
+    for (i in encoded_msg) {
+        text(encoded_msg[i], (8+(i*25)), 42);
+    }
+
+    var hook = document.getElementById("encodedMsg");
+    hook.textContent = "Encoded message: " + encoded_msg;
+    hook.style.color = "blue";
 }
 
 // Function to check validity of plaintext passwords
@@ -109,7 +107,7 @@ function isPasswordValid(plaintext)
     return (invalid == 0); // True if valid. False if invalid.
 }
 
-// Function to pad message with spaces if length less than 27.
+// Function to pad message with spaces and prepare str to encode.
 function prepareMessage(message, length)
 {
     if (message.length < 27) {message = (message + "                          ").substring(0,27);}
@@ -128,9 +126,7 @@ function prepareMessage(message, length)
     return (block_one + block_two + block_three + block_four + ascii_length_char);
 }
 
-//! SINGLE ENCRYPT FUNCTION DOESNT WORK FOR SOME VALUES
-//! SOME VALUES ARE OUT OF RANGE OF PRINTABLE ASCII CHARACTERS (32-127)
-//! EG ENCRYPT("t", "L") GIVES ASCII ENCRYPTED CHAR CODE OF 152. NOT IN RANGE.
+// function to encode a single character with a single pw character.
 function single_encrypt(msgChar, pwChar)
 {
     // first 4 chars of message -> 1st char of password etc.
@@ -139,29 +135,41 @@ function single_encrypt(msgChar, pwChar)
 
     ax = ax.charCodeAt(0) - 32;
     px = px.charCodeAt(0) - 32;
-    console.log("ax: " + ax + " | px: " + px);
+    //console.log("ax: " + ax + " | px: " + px);
 
     let bx = (ax ^ px); // bitwise XOR on ax and px
-    console.log("bx: " + bx);
+    //console.log("bx: " + bx);
     let cx = bx + 32;
-    console.log("cx: " + cx);
-    return String.fromCharCode(cx); // returns ascii code of encrypted char for message.
+    //console.log("cx: " + cx);
+    return String.fromCharCode(cx); // returns the character from ascii code.
 }
 
+// function to encode an entire 32 character string with a 8 char long pw.
 function encrypt(message, password) 
 {
-	let indexC = 0;
-  let cap = 0;
-  for (var p of password)
-  {
-  	console.log("P CHAR: " + p);
-  	while (cap < 4)
-    {
-    	console.log("MESSAGE CHAR: " + message[indexC]);
-    	console.log(single_encrypt(message[indexC], p));
-      indexC++;
-      cap++;
+    let indexC = 0;
+    let cap = 0;
+    let final_str = "";
+    for (var p of password) {
+        //console.log("P CHAR: " + p);
+        while (cap < 4) { // Change the password character to encode with every 4 characters.
+            //console.log("MESSAGE CHAR: " + message[indexC]);
+            //console.log(single_encrypt(message[indexC], p));
+            final_str = final_str.concat(single_encrypt(message[indexC], p));
+            indexC++;
+            cap++;
+        }
+        cap = 0;
     }
-    cap = 0;
-  }
+    //console.log("FINAL MESSAGE: ||" + final_str + "||");
+    //console.log("FINAL LEN: " + final_str.length);
+    return final_str; // the encoded message in a string.
+}
+
+function show_error(elem_id, msg)
+{
+    var error = document.getElementById(elem_id);
+    error.textContent = msg;
+    error.style.color = "red";
+    return;
 }
